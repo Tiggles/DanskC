99fa0959345e7db9c79af24ea4acc88e26804db0
Jonas Malte Hinchely <Tiggles@users.noreply.github.com>
2016-10-14 11:49:39 +0200

    Add files via upload

---
+++ dansk.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define heltal int
+#define svævende_punkt float
+#define lang long
+#define dobbel double
+#define imens while
+#define hvis if
+#define ellers else
+#define udskriv printf
+#define vend_tilbage return // Find bedre navn
+#define vend_tilbage_med return // Find bedre navn
+#define tegn char
+#define struktur struct
+#define øgenavn typedef
+#define når switch
+#define er case
+#define fortsæt continue
+#define afbryd break
+#define udgangspunkt default
+#define tælling enum
+#define tomrum void
+#define sandhedsværdi bool
+
+
+//#define \u200B NULL
